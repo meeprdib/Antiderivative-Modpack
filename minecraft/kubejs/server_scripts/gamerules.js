d8cd49a606ecbc1aca85049478451a2c1b4d67c9
Davey <meeprdib@outlook.com>
2024-12-30 02:13:12 -0500

    Worldgen stuff
    Removed wandering traders, removed uni's cave datapack, removed test iron veins

--- minecraft/kubejs/server_scripts/gamerules.js
+++ minecraft/kubejs/server_scripts/gamerules.js
@@ -2,4 +2,5 @@ ServerEvents.loaded(event => {
     event.server.gameRules.set("doInsomnia", false)
     event.server.gameRules.set("doPatrolSpawning", false)
     event.server.gameRules.set("disableRaids", true)
+    event.server.gameRules.set("doTraderSpawning", false)
 })

e0d03ec7ba48c712e26580798f953d70020a9c61
Davey <meeprdib@outlook.com>
2025-01-17 15:07:29 -0500

    refactor

---
+++ minecraft/kubejs/server_scripts/gamerules.js
@@ -0,0 +1,6 @@
+ServerEvents.loaded(event => {
+    event.server.gameRules.set("doInsomnia", false)
+    event.server.gameRules.set("doPatrolSpawning", false)
+    event.server.gameRules.set("disableRaids", true)
+    event.server.gameRules.set("doTraderSpawning", false)
+})
